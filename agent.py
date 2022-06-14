f76311deb72da84aaf2c5b47e40b5686de848bf4
Ryan Anderson <sonorousduck@gmail.com>
2022-06-13 19:08:27 -0600

    Found a good implementation of 2048

---
+++ agent.py
@@ -0,0 +1,17 @@
+class Agent:
+    def __init__(self):
+        pass
+
+    def move_down(self):
+        pass
+
+    def move_up(self):
+        pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+    
